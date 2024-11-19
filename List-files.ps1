6b4892a3fb99b6274e36a54af972681e756fc4f8
Rain <palemoon06@protonmail.com>
2024-11-19 19:22:09 +0100

    listFiles.ps1

--- List-files.ps1
+++ List-files.ps1
@@ -1,1 +1,4 @@
-Get-ChildItem . -Force -Path C:\ -Recurse -File | Sort-Object Length | Select-Object -Last 15 | Select-Object Name, @{Name="Size (GB)";Expression={"{0:N2}" -f ($_.Length/1GB)}}
+Get-ChildItem -Path C:\ -Recurse -File | `
+    Sort-Object Length | `
+    Select-Object -Last 15 | `
+    Select-Object Name, @{Name="Size (GB)";Expression={"{0:N2}" -f ($_.Length / 1GB)}}
